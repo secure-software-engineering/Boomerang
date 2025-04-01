fde00af8164dee2cba56e9567f03f9c873818eee
akshaya <akshayapuradam@gmail.com>
2025-04-01 19:23:50 +0200

    reverting TransitionFunctionOne

--- idealPDS/src/main/java/typestate/TransitionFunctionOne.java
+++ idealPDS/src/main/java/typestate/TransitionFunctionOne.java
@@ -11,18 +11,19 @@
  */
 package typestate;
 
-import static typestate.TransitionFunctionZero.zero;
-
 import boomerang.scope.ControlFlowGraph;
-import com.google.common.collect.Sets;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+
+import com.google.common.collect.Sets;
 import org.jspecify.annotations.NonNull;
 import typestate.finiteautomata.Transition;
 import typestate.finiteautomata.TransitionIdentity;
 import typestate.finiteautomata.TransitionImpl;
 import wpds.impl.Weight;
+
+import static typestate.TransitionFunctionZero.zero;
 
 public class TransitionFunctionOne implements TransitionFunction {
 
@@ -82,36 +83,8 @@ public class TransitionFunctionOne implements TransitionFunction {
     if (!(other instanceof TransitionFunction)) {
       throw new RuntimeException();
     }
-    TransitionFunctionZero zero = zero();
-    TransitionFunction one1 = one();
-
-    if (other == zero) return this;
-
-    if (other == one1 && this == one1) {
-      return one1;
+      return one();
     }
-
-    TransitionFunction func = (TransitionFunction) other;
-    if (other == (one1) || this == (one1)) {
-      Set<Transition> transitions =
-          new HashSet<>((other == (one1) ? getValues() : func.getValues()));
-      Set<Transition> idTransitions = Sets.newHashSet();
-      for (Transition t : transitions) {
-        idTransitions.add(new TransitionImpl(t.from(), t.from()));
-      }
-      transitions.addAll(idTransitions);
-      return new TransitionFunctionImpl(
-          transitions,
-          Sets.newHashSet(
-              (other == (one1) ? getStateChangeStatements() : func.getStateChangeStatements())));
-    }
-    Set<Transition> transitions = new HashSet<>(func.getValues());
-    transitions.addAll(getValues());
-    HashSet<ControlFlowGraph.Edge> newStateChangeStmts =
-        Sets.newHashSet(getStateChangeStatements());
-    newStateChangeStmts.addAll(func.getStateChangeStatements());
-    return new TransitionFunctionImpl(transitions, newStateChangeStmts);
-  }
 
   public String toString() {
     return "ONE";
