e0937eb845f29bb0deef3dbb1466d581bca15aa4
akshaya <akshayapuradam@gmail.com>
2025-04-01 20:07:28 +0200

    Fixing TransitionFunctionOne.java

--- idealPDS/src/main/java/typestate/TransitionFunctionOne.java
+++ idealPDS/src/main/java/typestate/TransitionFunctionOne.java
@@ -11,19 +11,17 @@
  */
 package typestate;
 
+import static typestate.TransitionFunctionZero.zero;
+
 import boomerang.scope.ControlFlowGraph;
+import com.google.common.collect.Sets;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.google.common.collect.Sets;
 import org.jspecify.annotations.NonNull;
 import typestate.finiteautomata.Transition;
-import typestate.finiteautomata.TransitionIdentity;
 import typestate.finiteautomata.TransitionImpl;
 import wpds.impl.Weight;
-
-import static typestate.TransitionFunctionZero.zero;
 
 public class TransitionFunctionOne implements TransitionFunction {
 
@@ -51,30 +49,10 @@ public class TransitionFunctionOne implements TransitionFunction {
   @NonNull
   @Override
   public Weight extendWith(@NonNull Weight other) {
-    TransitionFunctionOne one1 = one();
-    if (other == one1) return this;
-    if (this == one1) return other;
+    TransitionFunctionOne one = one();
+    if (other == one) return this;
+    return other;
 
-    TransitionFunction func = (TransitionFunction) other;
-    Set<? extends Transition> otherTransitions = (Set<? extends Transition>) func.getValues();
-    Set<Transition> ress = new HashSet<>();
-    Set<ControlFlowGraph.Edge> newStateChangeStatements = new HashSet<>();
-    for (Transition first : getValues()) {
-      for (Transition second : otherTransitions) {
-
-        if (second == (TransitionIdentity.identity())) {
-          ress.add(first);
-          newStateChangeStatements.addAll(getStateChangeStatements());
-        } else if (first == (TransitionIdentity.identity())) {
-          ress.add(second);
-          newStateChangeStatements.addAll(func.getStateChangeStatements());
-        } else if (first.to() == (second.from())) {
-          ress.add(new TransitionImpl(first.from(), second.to()));
-          newStateChangeStatements.addAll(func.getStateChangeStatements());
-        }
-      }
-    }
-    return new TransitionFunctionImpl(ress, newStateChangeStatements);
   }
 
   @NonNull
@@ -83,8 +61,31 @@ public class TransitionFunctionOne implements TransitionFunction {
     if (!(other instanceof TransitionFunction)) {
       throw new RuntimeException();
     }
-      return one();
+    TransitionFunctionZero zero = zero();
+    TransitionFunction one = one();
+
+    if (other == zero) return this;
+
+    if (other == one && this == one) {
+      return one;
     }
+
+    TransitionFunction func = (TransitionFunction) other;
+
+      Set<Transition> transitions =
+          new HashSet<>((other == (one) ? getValues() : func.getValues()));
+      Set<Transition> idTransitions = Sets.newHashSet();
+      for (Transition t : transitions) {
+        idTransitions.add(new TransitionImpl(t.from(), t.from()));
+      }
+      transitions.addAll(idTransitions);
+      return new TransitionFunctionImpl(
+          transitions,
+          Sets.newHashSet(
+              (other == (one) ? getStateChangeStatements() : func.getStateChangeStatements())));
+
+
+  }
 
   public String toString() {
     return "ONE";
